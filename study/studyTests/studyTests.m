037d9d04a309c24af28442c4b887659dfc844a5b
jianbo.bian <jianbo.bian@evdingding.com>
2017-06-15 15:22:27 +0800

    提交study

---
+++ study/studyTests/studyTests.m
@@ -0,0 +1,39 @@
+//
+//  studyTests.m
+//  studyTests
+//
+//  Created by pdid on 2017/6/15.
+//  Copyright © 2017年 pdid. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface studyTests : XCTestCase
+
+@end
+
+@implementation studyTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
